1e25b0dd8380cadb7c9d2ac08baf9f8d33057889
ephiguxta <fabiogustavo@tuta.io>
2024-04-15 09:35:38 -0400

    refatorando o código e adicionando a funcionalidade de nome dinâmico do bluetooth

--- udyat.ino
+++ udyat.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <BluetoothSerial.h>
+#include <string.h>
 
 // https://github.com/miguelbalboa/rfid
 #include <MFRC522.h>
@@ -8,15 +9,16 @@
 MFRC522 rfid(5, 27);
 
 BluetoothSerial SerialBT;
-const char device_name[32] = { 0 };
+char device_name[32] = "telemetria_";
 
 bool send_bluetooth_data(const char *log);
 bool check_data_request(void);
-void send_nibble(const char data);
+void nibble_to_byte(const char data, char conv_char[2]);
 char valid_char(const char data);
 bool check_cmd(const char *cmd);
 void error_log(const char *log);
-void get_uid(void);
+void get_uid(char uid[10]);
+void set_bt_name(char device_name[32]);
 
 void setup() {
   Serial.begin(115200);
@@ -24,9 +26,10 @@ void setup() {
 
   SPI.begin();
   rfid.PCD_Init();
-  rfid.PCD_AntennaOff();
 
-  SerialBT.begin(device_name);
+  delay(5000);
+
+  set_bt_name(device_name);
 
   delay(124);
 }
@@ -38,6 +41,7 @@ void loop() {
 
   // a tag rfid só será lida caso houver um comando bluetooth
   if(check_data_request()) {
+    Serial.printf("bluetooth name: [%s]\n", device_name);
     get_uid(uid);
     send_bluetooth_data(uid);
   }
@@ -60,7 +64,7 @@ bool check_cmd(const char *cmd) {
   int counter = 0;
 
   for(int i = 0; i < 16; i++) {
-    if(tmp[i] == '\0' || cmd[i] == '\0') {
+    if(tmp[i] == '\0' || cmd[i] == '\n') {
       break;
     }
 
@@ -68,6 +72,8 @@ bool check_cmd(const char *cmd) {
       counter++;
     }
   }
+
+  Serial.printf("cmd: [%s]\n", cmd);
 
   if(counter == 7) {
     return true;
@@ -115,7 +121,7 @@ bool send_bluetooth_data(const char *log) {
 
   for(int i = 0; i < 16; i++) {
     if(log[i] != '\0') {
-      send_nibble(log[i]);
+      SerialBT.write(log[i]);
       continue;
     }
 
@@ -141,7 +147,7 @@ char valid_char(const char data) {
   return conv;
 }
 
-void send_nibble(const char data) {
+void nibble_to_byte(const char data, char conv_char[2]) {
   // as máscaras servirão para dividir um
   // byte em dois bytes com o nibble mais significativo todo zerado,
   // então enviaremos dois bytes ao invés de um, para que
@@ -155,28 +161,39 @@ void send_nibble(const char data) {
   nibble &= 0x0f;
 
   nibble = valid_char(nibble);
-  SerialBT.write(nibble);
+  conv_char[0] = nibble;
 
   nibble = data;
   nibble &= 0x0f;
   nibble = valid_char(nibble);
-  SerialBT.write(nibble);
+  conv_char[1] = nibble;
 }
 
 void get_uid(char uid[10]) {
 
   // habilitando a antena para realizar a leitura do rfid
   rfid.PCD_AntennaOn();
+  rfid.PCD_AntennaOff();
+  rfid.PCD_AntennaOn();
 
   // verifica se o há a presença da tag e se ela pode
   // ser lida.
   if(rfid.PICC_IsNewCardPresent()) {
     if(rfid.PICC_ReadCardSerial()) {
+
       // quando chamamos o método ReadCardSerial, o PICC_SELECT
       // é chamado e as para pegar os bytes ocorrem
       for(short i = 0; i < rfid.uid.size; i++) {
-        uid[i] = rfid.uid.uidByte[i];
+        char buffer[2] = { 0 };
+        char uid_byte = rfid.uid.uidByte[i];
+
+        nibble_to_byte(uid_byte, buffer);
+        strncat(uid, buffer, 2);
       }
+
+      uid[(rfid.uid.size * 2) + 1] = '\0';
+
+      Serial.printf("Li o UID: [%s]\n", uid);
 
       rfid.PICC_HaltA();
       rfid.PCD_StopCrypto1();
@@ -188,4 +205,35 @@ void get_uid(char uid[10]) {
 
   rfid.PCD_AntennaOff();
   return;
+}
+
+void set_bt_name(char device_name[32]) {
+  delay(128);
+
+  char uid[10] = { 0 };
+  get_uid(uid);
+
+  Serial.printf("Peguei o UID, e é [%s]\n", uid);
+
+  unsigned int len = strlen(device_name);
+  Serial.printf("len: %d\n", len);
+
+  // esse campo rfid.uid.size é preenchido quando acessamos o
+  // get_uid, se ele for zero (aka falso) significa que nada
+  // foi lido
+  if(rfid.uid.size) {
+
+    strncat(device_name, uid, strlen(uid));
+
+    Serial.printf("O nome é valido: [%s]\n", device_name);
+
+  } else {
+    const char *error = "error";
+    for(int i = 0; i < 5; i++) {
+      device_name[i + len] = error[i];
+    }
+    Serial.printf("O nome é inválido: [%s]\n", device_name);
+  }
+
+  SerialBT.begin(device_name);
 }
